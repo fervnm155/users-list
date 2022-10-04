571a21d2749fc10e4a08089b3e1ae1ad35cfc221
Fernando Sustaita <fernandojsustaita@gmail.com>
2022-10-04 08:52:32 -0500

    users-list

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,13 @@
+
+
+const AddUser=(props)=>{
+    return(
+        <form>
+            <label>Username</label>
+            <input type="text" placeholder="Introduce user name"/>
+            <input type="number"/>
+        </form>
+    );
+}
+
+export default AddUser;
